bf8859537b4fba3dc5d49b6c93bb861c46534200
hjtb <willholtonjtb@gmail.com>
2022-07-11 19:12:05 +0000

    piped up templates and views

--- home/views.py
+++ home/views.py
@@ -4,3 +4,9 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+def club(request):
+    """ A view to return the index page """
+
+    return render(request, 'club.html')
