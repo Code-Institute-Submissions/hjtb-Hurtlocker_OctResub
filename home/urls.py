0b6b97306e6d95e4cb51648486bf625e7bdf8682
hjtb <willholtonjtb@gmail.com>
2022-07-20 21:04:04 +0000

    piping up the navbar with the different apps

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 
 
-# Remove unecessary paths - they might not be required anymore or else can be moved to urls in hurtlocker
 urlpatterns = [
     path('', views.index, name='home'),
-    path('club/', views.club, name='club'),
-    path('activities_page/', views.activities_page, name='activities'),
-    path('activity_page/<str:key>/', views.activity_page, name='activity_page'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,20 +1,6 @@
 from django.shortcuts import render
 
-# Remove unecessary views from here to their relevant apps
-
-members = [
-    {'member_id': 1, 'member_name': 'john'},
-    {'member_id': 2, 'member_name': 'paul'},
-    {'member_id': 3, 'member_name': 'jane'},
-]
-
-activities = [
-    {'activity_id': 1, 'activity_name': 'rugby'},
-    {'activity_id': 2, 'activity_name': 'football'},
-    {'activity_id': 3, 'activity_name': 'soccer'},
-    {'activity_id': 4, 'activity_name': 'hurling'},
-]
-
+# Pipe up the navbar with the various apps and pages
 
 def index(request):
     """ A view to return the index page """
@@ -29,39 +15,15 @@ def club(request):
     return render(request, 'club.html', context)
 
 
-def activities_page(request):
-    """ A view to return the activities page """
-    context = {'activities': activities}
-    return render(request, 'activities_page.html', context)
 
 
-def activity_page(request, key):
-    """ A view to return the selected activity page """
-    current_activity = None
-    for activity in activities:
-        if activity['activity_id'] == int(key):
-            current_activity = activity
-    context = {'current_activity': current_activity}
-    return render(request, 'activity_page.html', context)
+# def login(request):
+#     """ A view to return the login page """
+
+#     return render(request, 'login.html')
 
 
-# def profile(request, key):
-#     """ A view to return the profile page """
-#     current_profile = None
-#     for member in members:
-#         if member['member_id'] == int(key):
-#             current_profile = member
-#     context = {'current_profile': current_profile}
-#     return render(request, 'profile.html', context)
+# def register(request):
+#     """ A view to return the register page """
 
-
-def login(request):
-    """ A view to return the login page """
-
-    return render(request, 'login.html')
-
-
-def register(request):
-    """ A view to return the register page """
-
-    return render(request, 'register.html')
+#     return render(request, 'register.html')
