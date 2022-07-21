3005d7f110f951728f7acfd9ef9b1e2598d6bf6c
hjtb <willholtonjtb@gmail.com>
2022-07-21 20:43:44 +0000

    fixed duplicate databases in settings.py

--- hurtlocker/settings.py
+++ hurtlocker/settings.py
@@ -93,20 +93,6 @@ WSGI_APPLICATION = 'hurtlocker.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DATABASES = {
-    'default': dj_database_url.parse('')
-}
-
-# Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
