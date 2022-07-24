9cfa011bccca6013c8911fa16b5107f3aab4f187
hjtb <willholtonjtb@gmail.com>
2022-07-24 13:56:05 +0000

    models for activities and memberships updated

--- activities/models.py
+++ activities/models.py
@@ -4,10 +4,12 @@ from django.db import models
 
 
 class Activity(models.Model):
+    """Activity model to hold information regarding activities"""
 
     class Meta:
         verbose_name_plural = 'Activities'
-        
+
+    id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField(max_length=255, blank=True, null=True)
 
