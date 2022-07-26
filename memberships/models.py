9d0621b9d26aac7449e20866da4dbad8ac8d9d17
hjtb <willholtonjtb@gmail.com>
2022-07-26 20:10:30 +0000

    resetting deployed db

--- memberships/models.py
+++ memberships/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -6,12 +7,11 @@ from django.db import models
 class Membership(models.Model):
     """Membership model to hold information regarding memberships"""
 
-    class Meta:
-        verbose_name_plural = 'Activities'
-
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(max_length=255, blank=True, null=True)
+    number_of_activities = models.IntegerField(
+        validators=[MaxValueValidator(10), MinValueValidator(1)], blank=False, default=1)
 
     def __str__(self):
         return str(self.name)
