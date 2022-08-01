ca77e179c313e874d01ea45c10ae58651fc01e7f
hjtb <willholtonjtb@gmail.com>
2022-08-01 19:55:33 +0000

    created membership page

--- memberships/models.py
+++ memberships/models.py
@@ -7,11 +7,17 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class Membership(models.Model):
     """Membership model to hold information regarding memberships"""
 
+    class Meta:
+        ordering = ['number_of_activities']
+
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(max_length=255, blank=True, null=True)
     number_of_activities = models.IntegerField(
         validators=[MaxValueValidator(10), MinValueValidator(1)], blank=False, default=1)
+    image = models.ImageField(blank=True)
+    price = models.DecimalField(
+        max_digits=5, decimal_places=2, default=0, blank=False)
 
     def __str__(self):
         return str(self.name)
--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -15,8 +17,24 @@ class Profile(models.Model):
     last_name = models.CharField(max_length=30)
     bio = models.TextField(max_length=255, blank=True)
     signup_date = models.DateTimeField(auto_now_add=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(blank=True)
+
+    # billing information
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+    city = models.CharField(max_length=40, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
 
     def __str__(self):
         return str(self.user)
+
+    @receiver(post_save, sender=User)
+    def create_or_update_profile(sender, instance, created, **kwargs):
+        """
+        Create or update the user profile
+        """
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
