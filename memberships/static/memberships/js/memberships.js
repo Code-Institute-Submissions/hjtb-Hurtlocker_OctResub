b8e3cc0c504058d9febb04e7ef2fd55f2af96a55
hjtb <willholtonjtb@gmail.com>
2022-08-11 16:02:22 +0000

    Selecting checkboxes for use

--- memberships/static/memberships/js/memberships.js
+++ memberships/static/memberships/js/memberships.js
@@ -1,15 +1,19 @@
 $(document).ready(function () {
-    function ValidateActivitySelection() {
-        var checkboxes = document.document.getElementsByClassName("form-check-input");
-        console.log(checkboxes);
-        var numberOfCheckedItems = 0;
-        for (var i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].checked)
-                numberOfCheckedItems++;
-        }
-        if (numberOfCheckedItems > 2) {
-            alert("You can't select more than two activities!");
-            return false;
-        }
-    }
+    console.log("Before");
+    var checkboxes = document.getElementsByClassName("form-check-input");
+    console.log(checkboxes);
+    console.log("After");
+
+    // function ValidateActivitySelection() {
+    //     console.log(checkboxes);
+    //     var numberOfCheckedItems = 0;
+    //     for (var i = 0; i < checkboxes.length; i++) {
+    //         if (checkboxes[i].checked)
+    //             numberOfCheckedItems++;
+    //     }
+    //     if (numberOfCheckedItems > 2) {
+    //         alert("You can't select more than two activities!");
+    //         return false;
+    //     }
+    // }
 })
