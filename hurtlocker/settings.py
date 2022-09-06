14abea7b891b9c43b77a9d13ffecfa84f9d201d3
hjtb <willholtonjtb@gmail.com>
2022-09-06 21:11:36 +0000

    email configured

--- hurtlocker/settings.py
+++ hurtlocker/settings.py
@@ -106,7 +106,7 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -228,3 +228,16 @@ if 'STRIPE_ENDPOINT_SECRET' in os.environ:
     STRIPE_ENDPOINT_SECRET = os.environ.get('STRIPE_ENDPOINT_SECRET', '')
 else:
     STRIPE_ENDPOINT_SECRET = config('STRIPE_ENDPOINT_SECRET')
+
+
+if 'EMAIL_HOST_USER' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = 'info@hurtlocker.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'info@hurtlocker.com'
